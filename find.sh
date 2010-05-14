e76f8060f2057ab54a0352a3407ae749f4173bc9
Bernd Schubert <bernd.schubert@fastmail.fm>
2010-05-15 01:20:19 +0200

    Add new tests (find and tar)

---
+++ find.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source source_me.sh
+
+while [ 1 -eq 1 ]; do
+        find  ${TESTROOT_PARALLEL} >/dev/null
+done
+
---
+++ tar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source source_me.sh
+
+while [ 1 -eq 1 ]; do
+        tar cvf -  ${TESTROOT_PARALLEL} >/dev/null
+done
+
